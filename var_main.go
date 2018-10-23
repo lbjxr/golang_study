dc74f908c548c33e5dbf252808bf32238d594853
xr <1172907815@qq.com>
2018-10-23 22:15:59 +0800

    var use

---
+++ var_main.go
@@ -0,0 +1,17 @@
+package main
+
+//var v_name = value
+
+var a = "hello"
+
+//var v_name type = value
+
+var b string = " world"
+
+//var v_name value
+
+var c bool
+
+func main(){
+	println(a, b, c)
+}
