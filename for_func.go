eb70a7ddf150b2ec64bb0ae42185a8737129bd62
xr <1172907815@qq.com>
2018-11-03 20:37:28 +0800

    修改各个文件的函数名，测试gorouter轻线程

---
+++ for_func.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func for_func(a int , b int )  {
+
+		sum := a + b
+
+		fmt.Println(sum)
+}
--- mainFunc.go
+++ mainFunc.go
@@ -1,5 +1,17 @@
 package main
 
+import "time"
+
+//测试helloworld
 func main() {
-	go helloworld()
+	
+	//helloworld()
+
+	//测试gorouter 轻量级线程
+
+	for j := 0; j < 100; j++ {
+		go for_func(j,j + 1)
+	}
+
+	time.Sleep(10*time.Second)
 }
