3727a7d39fa681e307d829f467e2920fbcd322f9
xr <1172907815@qq.com>
2018-11-11 22:01:43 +0800

    math中的rand随机函数使用

---
+++ day02/example7/main/main.go
@@ -0,0 +1,29 @@
+package main
+
+import(
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init(){
+	//初始化种子
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main(){
+	for i := 0; i < 10; i++ {
+		a := rand.Int()
+		fmt.Println(a)	
+	}
+
+	for i := 0; i < 10; i++ {
+		a := rand.Intn(100)
+		fmt.Println(a)
+	}
+
+	for i := 0; i < 10; i++ {
+		a := rand.Float32()
+		fmt.Println(a)
+	}
+}
